cff4acb6f0ec0ce6e1b0b64d114f4f02a998d42c
EddieTheBill <dudumm1234@gmail.com>
2023-03-06 07:46:17 -0300

    first commit

---
+++ app/src/main/java/br/senai/sp/jandira/productlist/model/Product.kt
@@ -0,0 +1,14 @@
+package br.senai.sp.jandira.productlist.model
+
+class Product {
+
+    var id: Int = 0
+    var productName: String = ""
+    var productPrice: Double = 0.0
+    var productDescription: String = ""
+
+    fun getProductList() {
+        
+    }
+
+}
